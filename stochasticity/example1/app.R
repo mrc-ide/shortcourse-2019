4b5ee7db76dfda0d12b631e9e910dbd235d33b6b
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-08-13 13:56:21 +0100

    Add full set of stochastic examples

---
+++ stochasticity/example1/app.R
@@ -0,0 +1,7 @@
+if (file.exists("../../DESCRIPTION")) {
+  devtools::load_all("../..", export_all = FALSE, helpers = FALSE,
+                     attach_testthat = FALSE)
+}
+loadNamespace("odin.ui")
+
+odin.ui:::odin_stochastic("growth-deterministic.R")
